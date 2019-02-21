0eee6502ac2c65c275cf41f55a8918ca7c6b8e20
jeffyjkang <jeffyjkang@gmail.com>
2019-02-20 16:57:38 -0800

    climbing stairs

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -7,10 +7,11 @@
 # n = 1 would be 1
 # n = 2 would be 2
 # n = 3 would be 4
-# n = 4 would be 6
-# 1,1,1,1 / 2,1,1 / 1,2,1  / 1,1,2 / 3,1 / 1,3
-# n = 5 would be 10
-# fibonacci amount of ways
+# n = 4 would be 7
+# 1,1,1,1 / 2,1,1 / 1,2,1  / 1,1,2 / 2,2 / 3,1 / 1,3
+# n = 5 would be 13
+# 1,1,1,1 / 2,1,1,1 / 1,2,1,1 / 1,1,2,1 / 1,1,1,2 / 1,2,2 / 2,1,2 / 2,2,1 / 3,1,1 / 1,3,1/ 1,1,3 /2,3 / 3,2
+# fibonacci amount of ways but altered
 
 import sys
 
@@ -18,17 +19,18 @@ cache = {}
 
 
 def climbing_stairs(n, cache=None):
-    # if n == 3:
-    #     return 4
-    # if n == 2:
-    #     return 2
-    # if n == 1:
-    #     return 1
-    # if n <= 0:
-    #     return 0
-    # n = n * 3
-    # return climbing_stairs(n-1) + climbing_stairs(n-2)
-    pass
+    if n == 3:
+        return 4
+    if n == 2:
+        return 2
+    if n == 1:
+        return 1
+    if n <= 0:
+        return 0
+    return climbing_stairs(n-1) + climbing_stairs(n-2) + climbing_stairs(n-3)
+
+
+print(climbing_stairs(10))
 
 
 if __name__ == "__main__":
